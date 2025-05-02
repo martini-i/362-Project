836146fb10478540a0d0d004bf1263164d47bbe7
martini-i <martinnguyen34@csu.fullerton.edu>
2025-05-01 19:22:07 -0700

    Combined login and signup page for better usability.

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,25 +1,46 @@
-import React from 'react'
-import './CSS/LoginSignup.css'
+import React, { useState } from 'react';
+import './CSS/LoginSignup.css';
 
 export const LoginSignup = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
   return (
-    <div className='loginsignup'>
-      <div className="loginsignup-container">
-        <h1>Sign Up</h1>
-        <div className="loginsignup-fields">
-          <input type="text" placeholder='Your Name'/>
-          <input type="email" placeholder='Email Address' />
-          <input type="password" placeholder='Password' />
+    <div className="loginsignup">
+      <div className="loginsignup-container side-by-side">
+
+        {/* Login Box */}
+        <div className="loginsignup-box">
+          <h1>Login</h1>
+          <div className="loginsignup-fields">
+            <input type="email" placeholder="Email Address" />
+            <input type="password" placeholder="Password" />
+          </div>
+          <button>Login</button>
+          <p>
+          </p>
         </div>
-        <button>Continue</button>
-        <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
-        <div className="loginsignup-agree">
-          <input type="checkbox" name='' id='' />
-          <p>By continuing, I agree to the terms of use & privacy policy.</p>
+
+        {/* Sign Up Box */}
+        <div className="loginsignup-box">
+          <h1>Sign Up</h1>
+          <div className="loginsignup-fields">
+            <input type="text" placeholder="Your Name" />
+            <input type="email" placeholder="Email Address" />
+            <input type="password" placeholder="Password" />
+          </div>
+          <div className="loginsignup-bottom">
+            <button>Sign Up</button>
+            <div className="loginsignup-agree">
+              <input type="checkbox"/>
+              <p> By continuing, I agree to the terms of use & privacy policy.</p>
+            </div>
+          </div>
+          <p>
+          </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginSignup
+export default LoginSignup;
